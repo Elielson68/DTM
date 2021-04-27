bcf982f40d798e2f3c9abcbb182741a038cdd6d1
Elielson68 <elielsonbr.com@gmail.com>
2021-04-27 18:49:33 -0300

    Backend ASP NET

---
+++ back-end/Controllers/PatientController.cs
@@ -0,0 +1,45 @@
+﻿using DTMBackend.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Threading.Tasks;
+
+namespace DTMBackend.Controllers
+{
+    [ApiController]
+    
+    public class PatientController: ControllerBase
+    {
+        List<Patient> _listPatient = new List<Patient>()
+        {
+            new Patient(){Nome="Elielson", Idade=22},
+            new Patient(){Nome="Fernando", Idade=23}
+        };
+
+        [HttpGet]
+        [Route("api/[controller]")]
+        public IActionResult Get()
+        {
+            if(_listPatient.Count == 0)
+            {
+                return NotFound("Vazio");
+            }
+            return Ok(_listPatient);
+        }
+
+        [HttpGet]
+        [Route("api/[controller]/{nome}")]
+        public IActionResult Get(string nome)
+        {
+            Patient pacientFind = _listPatient.Find(x => x.Nome == nome);
+            if (pacientFind == null)
+            {
+                return NotFound("Paciente não encontrado");
+            }
+            return Ok(pacientFind);
+        }
+    }
+}
---
+++ back-end/Models/Patient.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTMBackend.Models
+{
+    public class Patient
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+    }
+}
