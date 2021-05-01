d736037b53383f3dce8efa38775f24ea091c80aa
Elielson68 <elielsonbr.com@gmail.com>
2021-04-30 22:13:21 -0300

    feat: models Exam, Patient and Users

---
+++ back-end/Models/Exam.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTMBackend.Models
+{
+    public class Patient
+    {
+        public string Date { get; set; }
+        public double OpenMeasurementPx {get; set;}
+        public double ShutMeasurementPx  {get; set;}
+        public double ResultMeasurementCm {get; set;}
+    }
+}
--- back-end/Models/Users.cs
+++ back-end/Models/Users.cs
@@ -10,10 +10,7 @@ namespace DTMBackend.Models
     {
         public string Name { get; set; }
         public string Email { get; set; }
-        public int Phone { get; set; }
-        public int Age { get; set; }
-        public string Phone { get; set; }
-        public string PainChoice { get; set; }
-        public double InitialDistanceCm {get; set;}
+        public string RegisteredNumber { get; set; }
+        public string Password { get; set; }
     }
 }
