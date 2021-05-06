d6dbe0bd4f899734416949132b41ae2c8b357b8b
lucasxxi <lucasleal2001@gmail.com>
2021-05-06 00:37:24 -0300

    refactor/fix: components names reviewed and fixed responsiveness

--- front-end/src/views/SignIn/index.tsx
+++ front-end/src/views/SignIn/index.tsx
@@ -1,29 +1,27 @@
 import React from "react";
-import Button from "@material-ui/core/Button";
-import { ArticleText, Container, HeaderBody, HeaderTop,Image,TextDTM, TextEvaluation } from "./styles";
+import { Body, Container, TextsDiv, LoginBox, TopHeader } from "./styles";
 
 const SignIn: React.FC = () => (
- <Container>
-    <HeaderTop>
-      <TextDTM>DTM</TextDTM>
-      <TextEvaluation>evaluation</TextEvaluation>
-    </HeaderTop>
-    <Image>
-    <Button variant="contained" color="primary">
-      Hello World
-    </Button>
-    <HeaderBody>
-      <h3>Conheça mais sobre o projeto DTM</h3>
-      <ArticleText>
-        <p >
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam molestie laoreet finibus. Vestibulum sodales consequat magna at placerat. Aenean sagittis erat eu iaculis ultricies. Proin at nibh enim. Nullam ac ornare lacus, at luctus quam. Phasellus scelerisque odio purus, id fermentum tellus interdum a. Donec efficitur metus eget quam tempor facilisis. Nam tincidunt commodo ex nec varius. 
+  <Body>
+    <TopHeader>
+        <h1>DTM</h1>
+        <span>evaluation</span>
+    </TopHeader>
+    <Container>
+        <TextsDiv>
+          <h3>Conheça mais sobre o projeto DTM</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+            molestie laoreet finibus. Vestibulum sodales consequat magna at
+            placerat. Aenean sagittis erat eu iaculis ultricies. Proin at nibh
+            enim. Nullam ac ornare lacus, at luctus quam. Phasellus scelerisque
+            odio purus, id fermentum tellus interdum a. Donec efficitur metus eget
+            quam tempor facilisis. Nam tincidunt commodo ex nec varius.
           </p>
-    </ArticleText>
-    </HeaderBody>
-    </Image>
-  </Container>
+        </TextsDiv>
+      <LoginBox />
+    </Container>
+  </Body>
 );
-
-
 
 export default SignIn;
--- front-end/src/views/SignIn/styles.ts
+++ front-end/src/views/SignIn/styles.ts
@@ -1,61 +1,79 @@
 import styled from "styled-components";
 import signInImage from "../../assets/background-signIn2.jpg";
 
-export const Container = styled.div`
-
-`;
-
-export const HeaderTop = styled.header`
+export const TopHeader = styled.header`
   background-color: #ba324f;
   color: white;
-  height: 5.5rem; 
+  height: 70px;
   display: flex;
   flex-direction: column;
   justify-content: center;
   padding-left: 30px;
+  
+  h1{
+    color: white;
+  }
+
+  span{
+    padding-left: 34px;
+    margin-top: -10px;
+  }
 `;
 
-export const TextDTM = styled.h1`
-  color: white;
+
+export const TextsDiv = styled.div`
+  color: #ffffff;
+  width:500px;
+  justify-self: center;
+
+  h3{
+    font-size: 3rem;
+  }
+
+  p{
+    margin-top: 17px;
+    font-size: 16px;
+    line-height: 19px;
+    font-weight: 700;
+  }
 `;
 
-export const TextEvaluation = styled.span`
-padding-left: 34px;
-margin-top: -10px;
-`
-
-export const Image = styled.div`
-background-image: url(${signInImage});
-background-position: center;
-background-repeat: no-repeat;
-background-size: cover;
-min-height: 100vh;
-`
-
-export const HeaderBody = styled.header`
-position: absolute;
-width: 483px;
-height: 124px;
-left: 100px;
-top: 190px;
-
-font-family: Roboto;
-font-style: normal;
-font-weight: bold;
-font-size: 45px;
-line-height: 58.59px;
-
-color: #FFFFFF;
+export const LoginBox = styled.div`
+  width: 500px;
+  height: 500px;
+  background: #ffffff;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 16px;
+  justify-self: center;
 
 `
 
-export const ArticleText = styled.article`
-margin-top: 17px;
-font-size: 16px;
-line-height: 19px;
-font-weight: 700;
-position: absolute;
-width: 518px;
-height: 157px;
-position: absolute;
-`
+export const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  padding: 2.5vw 5vw 0vw 5vw;
+
+  @media(max-width: 1080px){
+  display: flex;
+  flex-direction: column-reverse;
+  align-items: center;
+
+  ${LoginBox}{
+    width: 460px;
+  }
+
+  ${TextsDiv}{
+    display: none;
+  }
+}
+`;
+
+export const Body = styled.div`
+  background-image: url(${signInImage});
+  background-position: center center;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: cover;
+  height:100vh;
+`;
