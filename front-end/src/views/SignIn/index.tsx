e9bfa05f624ba0b1ef5a5b8e9f3cb0e24090012d
lucasxxi <lucasleal2001@gmail.com>
2021-05-17 22:27:23 -0300

    feat: LoginBox estilization and fixes.

--- front-end/src/views/SignIn/index.tsx
+++ front-end/src/views/SignIn/index.tsx
@@ -1,25 +1,64 @@
 import React from "react";
-import { Body, Container, TextsDiv, LoginBox, TopHeader } from "./styles";
+import {
+  Body,
+  Container,
+  TextsDiv,
+  LoginBox,
+  TopHeader,
+  TopEnterAccount,
+  InputsLogin,
+  UsersInput,
+  LoginButton,
+  ButtonDiv,
+  BottomMessage,
+  ForgotPass,
+} from "./styles";
 
 const SignIn: React.FC = () => (
   <Body>
     <TopHeader>
-        <h1>DTM</h1>
-        <span>evaluation</span>
+      <h1>DTM</h1>
+      <span>evaluation</span>
     </TopHeader>
     <Container>
-        <TextsDiv>
-          <h3>Conheça mais sobre o projeto DTM</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-            molestie laoreet finibus. Vestibulum sodales consequat magna at
-            placerat. Aenean sagittis erat eu iaculis ultricies. Proin at nibh
-            enim. Nullam ac ornare lacus, at luctus quam. Phasellus scelerisque
-            odio purus, id fermentum tellus interdum a. Donec efficitur metus eget
-            quam tempor facilisis. Nam tincidunt commodo ex nec varius.
-          </p>
-        </TextsDiv>
-      <LoginBox />
+      <TextsDiv>
+        <h3>Conheça mais sobre o projeto DTM</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+          molestie laoreet finibus. Vestibulum sodales consequat magna at
+          placerat. Aenean sagittis erat eu iaculis ultricies. Proin at nibh
+          enim. Nullam ac ornare lacus, at luctus quam. Phasellus scelerisque
+          odio purus, id fermentum tellus interdum a. Donec efficitur metus eget
+          quam tempor facilisis. Nam tincidunt commodo ex nec varius.
+        </p>
+      </TextsDiv>
+      <LoginBox>
+        <TopEnterAccount>
+          <h3>ENTRE COM A SUA CONTA</h3>
+        </TopEnterAccount>
+        <InputsLogin>
+          <UsersInput
+            id="outlined-basic"
+            label="E-mail"
+            placeholder="E-mail"
+            variant="outlined"
+          />
+          <UsersInput
+            id="outlined-basic"
+            label="Senha"
+            placeholder="Senha"
+            variant="outlined"
+            type="password"
+          />
+          <ForgotPass>Esqueci minha senha</ForgotPass>
+        </InputsLogin>
+        <ButtonDiv>
+          <LoginButton variant="contained" color="primary" size="large">
+            ENTRAR
+          </LoginButton>
+          <BottomMessage>Não tenho uma conta</BottomMessage>
+        </ButtonDiv>
+      </LoginBox>
     </Container>
   </Body>
 );
--- front-end/src/views/SignIn/styles.ts
+++ front-end/src/views/SignIn/styles.ts
@@ -1,3 +1,5 @@
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 import styled from "styled-components";
 import signInImage from "../../assets/background-signIn2.jpg";
 
@@ -9,32 +11,34 @@ export const TopHeader = styled.header`
   flex-direction: column;
   justify-content: center;
   padding-left: 30px;
-  
-  h1{
+  border-bottom: 7px solid white;
+
+  h1 {
     color: white;
   }
 
-  span{
+  span {
     padding-left: 34px;
     margin-top: -10px;
   }
 `;
 
-
 export const TextsDiv = styled.div`
   color: #ffffff;
-  width:500px;
+  width: 500px;
   justify-self: center;
 
-  h3{
-    font-size: 3rem;
+  h3 {
+    font-size: 3.7rem;
+    width: 30vw;
   }
 
-  p{
-    margin-top: 17px;
-    font-size: 16px;
+  p {
+    margin-top: 19px;
+    font-size: 1.4rem;
     line-height: 19px;
-    font-weight: 700;
+    font-weight: 800;
+    width: 30vw;
   }
 `;
 
@@ -43,30 +47,111 @@ export const LoginBox = styled.div`
   height: 500px;
   background: #ffffff;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-  border-radius: 16px;
+  border-radius: 15px 15px 15px 15px;
   justify-self: center;
+  align-items: center;
+`;
 
-`
+export const UsersInput = styled(TextField)`
+  &.MuiTextField-root {
+    margin-bottom: 15px;
+    margin-top: 15px;
+    height: 5vh;
+    width: 450px;
+    font-size: 1.1rem;
+  }
+`;
+
+export const InputsLogin = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 15px;
+`;
+
+export const ForgotPass = styled.div`
+  color: #777777;
+  width: 29%;
+  font-size: 14px;
+  transition: ease-in 1s;
+
+  :hover {
+    border-bottom: 1px solid gray;
+  }
+`;
+
+export const ButtonDiv = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+export const LoginButton = styled(Button)`
+  width: 75%;
+
+  &.MuiButton-root {
+    background-color: #f1ae02;
+    border-radius: 15px;
+    margin-top: 85px;
+    font-size: 1.2rem;
+
+    :hover {
+      background-color: #f29e02;
+    }
+  }
+`;
+
+export const BottomMessage = styled.div`
+  padding-top: 15px;
+  color: #777777;
+  font-size: 14px;
+
+  :hover {
+    border-bottom: 1px solid gray;
+  }
+`;
+
+export const TopEnterAccount = styled.div`
+  background: #102e4a;
+  border-radius: 15px 15px 0px 0px;
+
+  h3 {
+    font-size: 1.3rem;
+    padding: 31px 0 35px 0;
+    color: white;
+    text-align: center;
+  }
+`;
 
 export const Container = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: space-evenly;
   padding: 2.5vw 5vw 0vw 5vw;
+  margin-top: 5vh;
 
-  @media(max-width: 1080px){
-  display: flex;
-  flex-direction: column-reverse;
-  align-items: center;
+  @media (max-width: 1080px) {
+    display: flex;
+    flex-direction: column-reverse;
+    align-items: center;
 
-  ${LoginBox}{
-    width: 460px;
+    ${LoginBox} {
+      width: 460px;
+    }
+
+    ${TextsDiv} {
+      display: none;
+    }
+
+    ${UsersInput} {
+      &.MuiTextField-root {
+        width: 430px;
+      }
+    }
+    ${ForgotPass} {
+      width: 31%;
+    }
   }
-
-  ${TextsDiv}{
-    display: none;
-  }
-}
 `;
 
 export const Body = styled.div`
@@ -75,5 +160,5 @@ export const Body = styled.div`
   background-repeat: no-repeat;
   background-attachment: fixed;
   background-size: cover;
-  height:100vh;
+  height: 100vh;
 `;
