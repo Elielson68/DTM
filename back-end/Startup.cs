94d8f11580387b9d308b3b956a51dac43a428142
Elielson68 <elielsonbr.com@gmail.com>
2021-06-19 15:59:56 -0300

    feat: config project to use postgre

--- back-end/Startup.cs
+++ back-end/Startup.cs
@@ -1,14 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using DTMBackend.DataBase;
+using DTMBackend.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace DTMBackend
 {
     public class Startup
@@ -24,9 +21,10 @@ namespace DTMBackend
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
-            services.AddSingleton<IListPatients, ListPatients>();
-            services.AddSingleton<IListUsers, ListUsers>();
-            services.AddSingleton<IListExams, ListExams>();
+            services.AddMvc();
+            //Configuration.GetConnectionString("ContextConnectionString")
+            services.AddEntityFrameworkNpgsql().AddDbContext<AppDtmContext>(options => options.UseNpgsql(Configuration.GetConnectionString("ContextConnectionString")));
+            //services.AddApplicationInsightsTelemetry();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
