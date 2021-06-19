ed64addbdaf6868bf431822fd165f9f996e26d55
Elielson68 <elielsonbr.com@gmail.com>
2021-06-19 16:00:57 -0300

    refact: deleted interfaces and class list feat: crated app context to use postgre

---
+++ back-end/DataBase/AppDtmContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DTMBackend.Models
+{
+    public class AppDtmContext: DbContext
+    {
+        public AppDtmContext(DbContextOptions<AppDtmContext> options):base(options)
+        {
+        }
+        public DbSet<Patient> Patient { get; set; }
+        public DbSet<Users> Users { get; set; }
+        public DbSet<Exam> Exam { get; set; }
+    }
+}
