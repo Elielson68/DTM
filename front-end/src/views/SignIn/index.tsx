46714160bebaaaaa32224879009320dde614ae54
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2021-08-24 22:06:03 -0300

    login e modal de casdastro

--- front-end/src/views/SignIn/index.tsx
+++ front-end/src/views/SignIn/index.tsx
@@ -1,14 +1,70 @@
 import React from "react";
-import { Button } from "@material-ui/core";
-import { Container, Text } from "./styles";
+import RegisterModal from "../../components/RegisterModal";
+import {
+  AboutText,
+  Button,
+  Card,
+  Container,
+  ForgotPasswordText,
+  Form,
+  GetToKnowText,
+  Header,
+  HeaderBar,
+  Input,
+  Label,
+  LogoText,
+  MainContent,
+  RegisterText,
+  Texts,
+} from "./styles";
 
-const SignIn: React.FC = () => (
-  <Container>
-    <Text>SignIn</Text>
-    <Button variant="contained" color="primary">
-      Hello World!
-    </Button>
-  </Container>
-);
+export default function SignIn() {
+  const [show, setShow] = React.useState(false);
+  function handleClose() {
+    setShow(false);
+    return show;
+  }
+  return (
+    <Container>
+      <Header>
+        <LogoText>
+          <h1>DTM</h1>
+          <p>Evaluation</p>
+        </LogoText>
+      </Header>
+      <MainContent>
+        <Texts>
+          <GetToKnowText>Conheça mais sobre o projeto DTM</GetToKnowText>
+          <AboutText>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+            molestie laoreet finibus. Vestibulum sodales consequat magna at
+            placerat. Aenean sagittis erat eu iaculis ultricies. Proin at nibh
+            enim. Nullam ac ornare lacus, at luctus quam. Phasellus scelerisque
+            odio purus, id fermentum tellus interdum a. Donec efficitur metus
+            eget quam tempor facilisis. Nam tincidunt commodo ex nec varius.
+          </AboutText>
+        </Texts>
 
-export default SignIn;
+        <Card>
+          <HeaderBar>ENTRE COM A SUA CONTA</HeaderBar>
+          <Form>
+            <Label>E-mail</Label>
+            <Input placeholder="exemplo@email.com" type="email" />
+
+            <Label>Matricula</Label>
+            <Input placeholder="12345678" type="number" />
+            <ForgotPasswordText>Esqueci Minha Senha</ForgotPasswordText>
+            <Button>Entrar</Button>
+            <RegisterText onClick={() => setShow(true)}>
+              Não tenho uma conta
+            </RegisterText>
+            <RegisterModal
+              onClose={() => handleClose()}
+              show={show}
+            />
+          </Form>
+        </Card>
+      </MainContent>
+    </Container>
+  );
+}
