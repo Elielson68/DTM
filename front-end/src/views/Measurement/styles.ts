9063a0260079c9f80ce9a6d8b8dbd87b47e63485
beijallote@gmail.com <beijallote@gmail.com>
2021-09-14 15:24:22 -0300

    fix: responsiveness of the measuring screen and patient details

--- front-end/src/views/Measurement/styles.ts
+++ front-end/src/views/Measurement/styles.ts
@@ -1,41 +1,28 @@
 import styled from "styled-components";
-
 // Importando imagem dos assets
 import img from "../../assets/img/imgMeasurementCard.jpg";
 
 // Elemento Pai
 export const Container = styled.div`
   background-color: #102E4A;
-
-  width: 100vw;
-  height: 100vh;
-
   display: flex;
   flex-direction: row;
   justify-content: center;
-
   align-items: center;
-
-  @media(max-width: 1024px) {
-        & {
-            display: flex;
-            flex-direction: column;
-            height: 150vh;
-        }
-    }
+  margin: 0;
+  padding: 0;
+  height: 100vh;
+  @media(max-width: 1170px) {
+    flex-direction: column;
+    height: 100%;
+  }
 `;
-
 // Lado Esquerdo
 export const DivLeft = styled.div`
   h1 {
     color: #fff;
     padding: 10px;
     text-align: center;
-    @media(max-width: 1024px) {
-        & {
-            font-size: 35px;
-        }
-    }
   }
 `;
 export const CardLeft = styled.div`
@@ -44,62 +31,63 @@ export const CardLeft = styled.div`
   padding: 23px 0px 75px 0px;
   width: 490px;
   margin-right: 10px;
-
-  @media(max-width: 1024px) {
-    margin-left: 10px;
+  @media(max-width: 1170px) {
+    margin-right: 0;
+  }
+  @media(max-width: 490px) {
+    width: 400px;
+  }
+  @media(max-width: 425px) {
+    width: 300px;
+    padding: 20px 0px 70px 0px;
   }
   p {
     font-size: 15px;
     font-weight: bold;
     margin-left: 90px;
-
-    @media(max-width: 1024px) {
-        & {
-            font-size: 18px;
-        }
+    @media(max-width: 425px) {
+      text-align: center;
+      margin-left: 0px;
     }
   }
   h4 {
     text-align: center;
-    @media(max-width: 1024px) {
-        & {
-            font-size: 20px;
-        }
-    }
   }
 `;
 export const Div = styled.div`
   display: flex;
-  flex-direction: row;
+  flex: row wrap;
   justify-content: center;
 `;
 export const DivBox = styled.div`
   border: 1px solid #808080;
   border-radius: 15px;
-
   width: 150px;
   height: 40px;
-  
   margin-left: 10px;
+  @media(max-width: 425px) {
+    margin-left: 5px;
+    width: 100px;
+  }
 `;
 export const StartButton = styled.button`
   width: 150px;
   height: 40px;
   margin-right: 10px;
-
   border: none;
   border-radius: 15px;
-
   cursor: pointer;
   background-color: #009F92;
   text-align: center;
-
   color: #fff;
   font-weight: bold;
+  @media(max-width: 425px) {
+    margin-right: 0px;
+    width: 100px;
+  }
 `;
 export const Line = styled.div`
   border-bottom: 1px solid #808080;
-
   width: 60%;
   margin: 0 auto;
   padding: 5px;
@@ -108,67 +96,54 @@ export const FinishButton = styled.button`
   width: 70%;
   height: 40px;
   margin-top: 80px;
-
   border: none;
   border-radius: 15px;
-
   cursor: pointer;
   background-color: #F1AE02;
-
   color: #fff;
   font-weight: bold;
   font-size: 17px;
-
-  @media(max-width: 1024px) {
-        & {
-            font-size: 20px;
-            height: 47px;
-        }
-    }
 `;
-
 // Lado Direito
 export const DivRight = styled.div`
-  margin-left: 10px;
-   
   h1 {
     color: #fff;
     padding: 10px;
     text-align: center;
-    @media(max-width: 1024px) {
-        & {
-            font-size: 35px;
-        }
-    }
   }
 `;
 export const CardRight = styled.div`
   background-color: #fff;
-  padding: 10px;
+  padding: 25px;
   width: 590px;
   height: 490px;
   border-radius: 15px;
-  border: 10px solid #fff;
-  
-  @media(max-width: 1024px) {
-        & {
-            width: 490px;
-        }
+  margin-left: 10px;
+  @media(max-width: 1170px) {
+    margin-bottom: 250px;
+  }
+  @media(max-width: 665px) {
+    width: 500px;
+    padding: 20px;
+  }
+  @media(max-width: 565px) {
+    width: 400px;
+    padding: 15px;
+  }
+  @media(max-width: 442px) {
+    width: 300px;
+    padding: 10px;
+  }
+  @media(max-width: 330px) {
+    width: 260px;
   }
 `;
 export const Img = styled.div`
   background-image: url(${img});
   background-repeat: no-repeat;
   background-size: cover;
-  
   width: 100%;
   height: 100%;
   border-radius: 15px;
-
-  @media(max-width: 1024px) {
-    &{
-      align-items: center;
-      width: 100%;
-    }
-  }
+  align-items: center;
 `;
