7bbbed11f96c8102fecca00d4671daeb4f9276b4
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2021-09-14 08:36:48 -0300

    .

--- front-end/src/views/Dashboard/index.tsx
+++ front-end/src/views/Dashboard/index.tsx
@@ -1,11 +1,82 @@
 import React from "react";
 
-import { Container } from "./styles";
+import { Container, MainContent, UpperDiv, Search, Table } from "./styles";
 
-const Dashboard: React.FC = () => (
-    <Container>
-      Teste
-    </Container>
-);
+import SearchIcon from "../../assets/img/search_icon.png";
 
-export default Dashboard;
+export default function Dashboard() {
+  return (
+    <>
+      <Container>
+        <MainContent>
+          <UpperDiv>
+            <h2>Lista de Pacientes</h2>
+            <Search>
+              <input type="text" placeholder="Nome do paciente" />
+              <span>
+                <img src={SearchIcon} alt="" />
+              </span>
+            </Search>
+          </UpperDiv>
+          <Table>
+            <thead className="table-header">
+              <tr>
+                <th>Company</th>
+                <th>Contact</th>
+                <th>Country</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Alfreds Futterkiste</td>
+                <td>Maria Anders</td>
+                <td>Germany</td>
+              </tr>
+              <tr>
+                <td>Alfreds Futterkiste</td>
+                <td>Maria Anders</td>
+                <td>Germany</td>
+              </tr>
+              <tr>
+                <td>Alfreds Futterkiste</td>
+                <td>Maria Anders</td>
+                <td>Germany</td>
+              </tr>
+              <tr>
+                <td>Alfreds Futterkiste</td>
+                <td>Maria Anders</td>
+                <td>Germany</td>
+              </tr>
+              <tr>
+                <td>Alfreds Futterkiste</td>
+                <td>Maria Anders</td>
+                <td>Germany</td>
+              </tr>
+
+              <tr>
+                <td>Alfreds Futterkiste</td>
+                <td>Maria Anders</td>
+                <td>Germany</td>
+              </tr>
+              <tr>
+                <td>Alfreds Futterkiste</td>
+                <td>Maria Anders</td>
+                <td>Germany</td>
+              </tr>
+              <tr>
+                <td>Alfreds Futterkiste</td>
+                <td>Maria Anders</td>
+                <td>Germany</td>
+              </tr>
+              <tr>
+                <td>Alfreds Futterkiste</td>
+                <td>Maria Anders</td>
+                <td>Germany</td>
+              </tr>
+            </tbody>
+          </Table>
+        </MainContent>
+      </Container>
+    </>
+  );
+}
--- front-end/src/views/Dashboard/styles.ts
+++ front-end/src/views/Dashboard/styles.ts
@@ -1,5 +1,103 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  
+  width: 100%;
+  height: 100vh;
+  background-color: #102e4a;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const MainContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+  border: 1px solid red;
+  width: 80%;
+  height: 80%;
+`;
+
+export const UpperDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  height: fit-content;
+  border: 1px solid white;
+  margin-bottom: 3%;
+
+  h2 {
+    color: #ffffff;
+  }
+`;
+
+export const Search = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  width: fit-content;
+  height: fit-content;
+  border-bottom: 1px solid white;
+
+  input {
+    outline: none;
+    margin: none;
+    padding: 2%;
+    background: none;
+  }
+
+  span {
+    img {
+      width: 30px;
+      height: 30px;
+    }
+  }
+`;
+
+export const Table = styled.table`
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  background-color: #fff;
+  border-radius: 16px;
+
+  thead {
+    color: #000;
+    font-weight: bold;
+    font-size: 23px;
+    display: flex;
+    padding: 2%;
+    text-align: center;
+    tr {
+      display: flex;
+      width: 100%;
+      th {
+        width: 33.33%;
+      }
+    }
+  }
+
+  tbody{
+    overflow-y: scroll;
+    display: flex;
+    flex-direction: column;
+    max-height: 560px;
+    tr{
+      cursor: pointer;
+      transition: background-color 250ms ease-in-out;
+      display: flex;
+      width: 100%;
+      margin: 2% 0;
+      td{
+        width: 33.33%;
+      }
+    }
+  }
 `;
