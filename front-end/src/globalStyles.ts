224b3e516b299f57f28725ff3081c3ef0aeeb6fc
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2021-09-14 11:44:41 -0300

    removed unsed styling

--- front-end/src/globalStyles.ts
+++ front-end/src/globalStyles.ts
@@ -1,11 +1,15 @@
-import { createGlobalStyle } from 'styled-components';
- 
+import { createGlobalStyle } from "styled-components";
+
 const GlobalStyle = createGlobalStyle`
+  html, body {
+    width: 100vw;
+    height: 100vh;
+  }
   body {
     margin: 0;
     padding: 0;
     font-family: 'Roboto Condensed', sans-serif;
   }
 `;
- 
+
 export default GlobalStyle;
