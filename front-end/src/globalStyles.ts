effc4a0d49ff27fba425b986e1926f1331f4d089
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2021-09-15 20:20:02 -0300

    altered global styles

--- front-end/src/globalStyles.ts
+++ front-end/src/globalStyles.ts
@@ -9,12 +9,14 @@ const GlobalStyle = createGlobalStyle`
     font-family: 'Roboto Condensed', sans-serif;
   }
 
-  #root{
+  body{
     background: #102e4a;
+  }
+
+  #root{
     width: 100vw;
     max-width: 100vw;
     height: 100vh;
-    max-height: 100vh;
   }
 `;
 
