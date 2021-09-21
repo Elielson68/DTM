a72469690e2502f408b149aeea37da604140a4b4
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2021-09-21 12:18:52 -0300

    altered components positions

--- front-end/src/views/ExamData/index.tsx
+++ front-end/src/views/ExamData/index.tsx
@@ -14,52 +14,51 @@ import DummyPic from "../../assets/img/imgMeasurementCard.jpg";
 
 export default function ExamData() {
   return (
-    <>
+    <Container>
       <MenuBar />
-      <Container>
-        <MainContent>
-          <Exam>
-            <h1>Dados do Exame</h1>
-            <Data>
-              <TextInfo>
-                <span>Nome</span>
-                <h3>Rodrigo Silva</h3>
-              </TextInfo>
-              <TextInfo>
-                <span>Médico</span>
-                <h3>Dr. Manoel Pereira</h3>
-              </TextInfo>
-              <MeasurementInfo>
-                <span>Medição com a boca fechada</span>
-                <span>100 mm</span>
-              </MeasurementInfo>
-              <MeasurementInfo>
-                <span>Medição com a boca aberta</span>
-                <span>90 mm</span>
-              </MeasurementInfo>
-              <MeasurementInfo>
-                <span>Resultado</span>
-                <span>10 mm</span>
-              </MeasurementInfo>
-              <TextInfo>
-                <span>Data</span>
-                <h3>20/09/2020</h3>
-              </TextInfo>
-            </Data>
-          </Exam>
+      <MainContent>
+        <Exam>
+          <h1>Dados do Exame</h1>
+          <Data>
+            <TextInfo>
+              <span>Nome</span>
+              <h3>Rodrigo Silva</h3>
+            </TextInfo>
+            <TextInfo>
+              <span>Médico</span>
+              <h3>Dr. Manoel Pereira</h3>
+            </TextInfo>
+            <MeasurementInfo>
+              <span>Medição com a boca fechada</span>
+              <span>100 mm</span>
+            </MeasurementInfo>
+            <MeasurementInfo>
+              <span>Medição com a boca aberta</span>
+              <span>90 mm</span>
+            </MeasurementInfo>
+            <MeasurementInfo>
+              <span>Resultado</span>
+              <span>10 mm</span>
+            </MeasurementInfo>
+            <TextInfo>
+              <span>Data</span>
+              <h3>20/09/2020</h3>
+            </TextInfo>
+          </Data>
+        </Exam>
 
-          <Pictures>
+        <Pictures>
+          <Picture>
             <h1>Foto com a boca aberta</h1>
-            <Picture>
-              <img src={DummyPic} alt="" />
-            </Picture>
+            <img src={DummyPic} alt="" />
+          </Picture>
+
+          <Picture>
             <h1>Foto com a boca fechada</h1>
-            <Picture>
-              <img src={DummyPic} alt="" />
-            </Picture>
-          </Pictures>
-        </MainContent>
-      </Container>
-    </>
+            <img src={DummyPic} alt="" />
+          </Picture>
+        </Pictures>
+      </MainContent>
+    </Container>
   );
 }
